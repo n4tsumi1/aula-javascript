88aae7b0ed208eff987cdc5bdcc4fbb4242c11a4
Erika Queiroz <72478111+n4tsumi1@users.noreply.github.com>
2022-08-23 05:51:25 -0300

    Add files via upload

---
+++ mes-json.js
@@ -0,0 +1,36 @@
+// JavaScript source code
+/*Desafio
+Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, em inglês, com a primeira letra maiúscula.
+
+Entrada
+A entrada contém um único valor inteiro.
+
+Saída
+Imprima por extenso o nome do mês correspondente ao número existente na entrada, com a primeira letra em maiúscula.*/
+
+// IMPORTANTE: As funções "gets" e "print" são acessíveis globalmente e têm as seguintes funcionalidades: 
+// - "gets" : lê UMA linha com dados de entrada (inputs) do usuário;
+// - "print": imprime um texto de saída (output) e pula uma linha ("\n") automaticamente;
+// Abaixo segue o template de código para este desafio, o qual pode ou não utilizar tais funções.
+
+const monthNumber = gets();
+
+//TODO: Complete o JSON "months" com os meses faltantes.
+const months = {
+  "1": "January",
+  "2": "February",
+  "3": "March",
+  "4": "April",
+  "5": "May",
+  "6": "June",
+  "7": "July",
+  "8": "August",
+  "9": "September",
+  "10": "October",
+  "11": "November",
+  "12": "December"
+  };
+  
+
+
+print(months[monthNumber]);
